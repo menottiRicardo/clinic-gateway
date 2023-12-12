c6a22ab45d4a6bc2439eb20839cf2d94a38cf7e0
ricardomenotti <ricardomenotti31@gmail.com>
2023-12-11 19:29:26 -0500

    unused services

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 // import all the required packages
 const cors = require('cors');
 const express = require('express');
-const rateLimit = require('express-rate-limit');
-const expressWinston = require('express-winston');
 const helmet = require('helmet');
 const { createProxyMiddleware } = require('http-proxy-middleware');
-const responseTime = require('response-time');
-const winston = require('winston');
 const config = require('./config');
 
 // configure the application
@@ -31,26 +27,7 @@ app.disable('x-powered-by');
 
 app.use(helmet());
 
-app.use(responseTime());
-
-app.use(
-  expressWinston.logger({
-    transports: [new winston.transports.Console()],
-    format: winston.format.json(),
-    statusLevels: true,
-    meta: false,
-    level: 'debug',
-    msg: 'HTTP {{req.method}} {{req.url}} {{res.statusCode}} {{res.responseTime}}ms',
-    expressFormat: true,
-    ignoreRoute() {
-      return false;
-    },
-  })
-);
-
 app.use(cors());
-
-app.use(rateLimit(config.rate));
 
 Object.keys(config.proxies).forEach((path) => {
   const { protected, ...options } = config.proxies[path];
